c7c96bf425947cd2f8efc36f0a05c4754448308c
DedNikolai <nikolai.blashchuk@gmail.com>
2020-05-20 21:48:21 +0300

    create home page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.feedbacks-slider').slick({
+        // dots: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+});
+});
