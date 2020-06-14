13ba7c49ab358931111cf455b64236822f949e1c
DedNikolai <nikolai.blashchuk@gmail.com>
2020-06-14 13:16:22 +0300

    add responsive design

--- js/script.js
+++ js/script.js
@@ -7,5 +7,36 @@ $(document).ready(function(){
         centerMode: true,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
 });
+    $('.burger-toggler').click(() => {
+        $('.navbar').show( 300 );
+    })
+
+    $('.close').click(() => {
+        $('.navbar').hide(300)
+    })
+
+    $('.navigation__link').click(() => {
+        $('.navbar').hide(300)
+    })
 });
